f9cbf5570bc1046f1718c4ce1270c7f2d26575e2
Aalya Banu <aalya.banu@gmail.com>
2020-10-27 07:40:13 +0000

    Updates

---
+++ precache-manifest.3aec834e1e7e009faff76cd10f107daa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96377e62a4959ae96b6f9fef6a452845",
+    "url": "/Blogosphere/index.html"
+  },
+  {
+    "revision": "974ad9deefb794cf6e56",
+    "url": "/Blogosphere/static/css/main.0759322e.chunk.css"
+  },
+  {
+    "revision": "a371ea2bb81eaad69b84",
+    "url": "/Blogosphere/static/js/2.5550772e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Blogosphere/static/js/2.5550772e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "974ad9deefb794cf6e56",
+    "url": "/Blogosphere/static/js/main.39522539.chunk.js"
+  },
+  {
+    "revision": "da39dfcea2ded4b0b0dd",
+    "url": "/Blogosphere/static/js/runtime-main.2f0a70c6.js"
+  }
+]);
